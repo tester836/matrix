92c336e390c443c136d749b69beb5b203efb3ee9
anton.pashutkin <anton.pashutkin@gxbventures.com>
2022-02-28 13:24:11 +0300

    added exception for constraints, WIP inverse matrix and determinant

--- src/main/java/com/epam/tat/matrixprocessor/impl/Main.java
+++ src/main/java/com/epam/tat/matrixprocessor/impl/Main.java
@@ -1,32 +1,44 @@
 package com.epam.tat.matrixprocessor.impl;
 
+import com.epam.tat.matrixprocessor.exception.MatrixProcessorException;
+
 public class Main {
 
     public static void main(String args[]) {
         MatrixProcessor matrixProcessor = new MatrixProcessor();
 
-        double[][] matrix = {{1.1, 0.3}, {-2.03, 0.0}, {3.456, 33.6543}};
+        double[][] matrix = {{-3.1244, 15.5}, {5.01, -7.14}, {-2.12, 3.4}};
         double[][] firstMatrix = {{1.1, 0}, {-2.12, 3.4}};
         double[][] secondMatrix = {{-3.1244, 15.5}, {5.01, -7.14}};
+        int maxRows = 10;
+        int maxColumns = 10;
+        int minRows = 1;
+        int minColumns = 1;
 
-        System.out.println("\n\tThe original matrix is:\n" +
-                matrixProcessor.displayMatrix(matrix));
+        if (matrix.length < maxRows & matrix[0].length > minRows &
+                matrix[0].length < maxColumns &  matrix[0].length > minColumns) {
+            System.out.println("\n\tThe original matrix is:\n" +
+                    matrixProcessor.displayMatrix(matrix));
 
-        System.out.println("\tThe transposed matrix is:\n" +
-                matrixProcessor.displayMatrix(matrixProcessor.transpose(matrix)));
+            System.out.println("\tThe transposed matrix is:\n" +
+                    matrixProcessor.displayMatrix(matrixProcessor.transpose(matrix)));
 
-        System.out.println("\tThe turned clockwise matrix is:\n" +
-                matrixProcessor.displayMatrix(matrixProcessor.turnClockwise(matrix)));
+            System.out.println("\tThe turned clockwise matrix is:\n" +
+                    matrixProcessor.displayMatrix(matrixProcessor.turnClockwise(matrix)));
 
-        System.out.println("\tThe multiplication of matrices is:\n" +
-                matrixProcessor.displayMatrix(matrixProcessor.multiplyMatrices(firstMatrix, secondMatrix)));
+            System.out.println("\tThe multiplication of matrices is:\n" +
+                    matrixProcessor.displayMatrix(matrixProcessor.multiplyMatrices(firstMatrix, secondMatrix)));
 
 
-        System.out.println("\tThe inversed matrix is:\n" +
-                matrixProcessor.displayMatrix(matrixProcessor.getInverseMatrix(matrix)));
+            System.out.println("\tThe inversed matrix is:\n" +
+                    matrixProcessor.displayMatrix(matrixProcessor.getInverseMatrix(matrix)));
 
-        System.out.println("\tThe matrix determinant is:\n" +
-                matrixProcessor.getMatrixDeterminant(matrix));
+            System.out.println("\tThe matrix determinant is:\n" +
+                    matrixProcessor.getMatrixDeterminant(matrix));
+        }
+        else System.out.println("Error: Number of rows and columns in matrix should be from 1 to 9."); {
+            throw new MatrixProcessorException();
+        }
 
     }
 
